9e74c7bced9814902a38fe43f762cd778641c9e4
Feather <longfeatherzhong@gmail.com>
2019-04-19 22:05:11 +0800

    Add cart

---
+++ src/main/java/edu/ycu/shopping/entity/Cart.java
@@ -0,0 +1,43 @@
+package edu.ycu.shopping.entity;
+
+public class Cart {
+    private Integer id;
+
+    private Integer goodId;
+
+    private Integer userId;
+
+    private Integer count;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getGoodId() {
+        return goodId;
+    }
+
+    public void setGoodId(Integer goodId) {
+        this.goodId = goodId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+}
